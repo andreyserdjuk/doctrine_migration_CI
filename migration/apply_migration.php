08842b934496d3db56d7d489a01b63624d8b2fde
Andrej Serdjuk <ase6120@gmail.com>
2014-07-13 21:06:07 +0300

    refactored, added db_running field

--- migration/apply_migration.php
+++ migration/apply_migration.php
@@ -2,58 +2,63 @@
 
 require_once dirname(__FILE__) . '/../envvars.php';
 require_once dirname(__FILE__) . '/../libraries/Doctrine.php';
-$doctrine = new Doctrine;
+$doctrine = new \Doctrine;
 
 // get migrations files list
-$migrationsFiles = scandir(dirname(__FILE__) . '/dumps', SCANDIR_SORT_DESCENDING);
+$migrFiles = scandir(dirname(__FILE__) . '/dumps', SCANDIR_SORT_DESCENDING);
 
-// get current db version
-$dbVersion = $doctrine->em->getRepository('models\LocalOffice')->findOneByKey('db_version');
+if (!empty($migrFiles)) {
+	// remove directory dots:
+	$migrFiles = array_slice($migrFiles, 0, -2);
 
-// not found version record? - create it as last dump
-if (!$dbVersion) {
-	$dbVersion = new models\LocalOffice;
-	$dbVersion->setKey('db_version');
-	$dbVersion->setType('string');
+	// get current db_version
+	$dbVersion = $doctrine->em->getRepository('models\LocalOffice')->findOneByKey('db_version');
+	$dbRunning = $doctrine->em->getRepository('models\LocalOffice')->findOneByKey('updater_running');
 
-	// take last dump and set as last version
-	foreach ($migrationsFiles as $fileName) {
-		if (preg_match('/^\d+\.php$/', $fileName)) {
-			$dbVersion->setValue(substr($fileName, 0, -4));
-			break;
-		}
+	if (!$dbRunning) {
+		$dbRunning = new \models\LocalOffice;
+		$dbRunning->setKey('updater_running');
+		$dbRunning->setType('integer');
+		$doctrine->em->persist($dbRunning);
 	}
-	$doctrine->em->persist($dbVersion);
-    $doctrine->em->flush();
-    // I think, initially we have one migration file, remove directory dots:
-    $migrationsFiles = array_slice($migrationsFiles, 0, 1);
-} else {
-	// get migrations order for apply
-	array_splice($migrationsFiles, array_search($dbVersion->getValue() . '.php', $migrationsFiles));
-}
 
-$conn = $doctrine->em->getConnection();
-if (!empty($migrationsFiles)) {
-	for ($i=count($migrationsFiles); $i >= 0; $i--) { 
+	// all applications should to know, that schema update running
+	$dbRunning->setValue('1');
+	$doctrine->em->flush();
+
+	if (!$dbVersion) {
+		// not found db_version? - create it as last dump
+		$dbVersion = new \models\LocalOffice;
+		$dbVersion->setKey('db_version');
+		$dbVersion->setType('string');
+		$doctrine->em->persist($dbVersion);
+	} else {
+		// get migrations order for apply
+		array_splice($migrFiles, array_search($dbVersion->getValue() . '.php', $migrFiles));
+	}
+
+	$conn = $doctrine->em->getConnection();
+	sort($migrFiles, SORT_NUMERIC);
+
+	foreach ($migrFiles as $f) {
 		try {
-			if (isset($migrationsFiles[$i])) {
-				$migrationFile = $migrationsFiles[$i];
-				require_once dirname(__FILE__) . '/dumps/' . $migrationFile;
-				$migrationMark = substr($migrationFile, 0, -4);
-				$queriesVariable = 'queries_' . $migrationMark;
-				foreach ($$queriesVariable as $q) {
-					$conn->executeQuery('SET FOREIGN_KEY_CHECKS=0');
-					$conn->executeQuery($q);
-				}
-				$dbVersion->setValue($migrationMark);
-				$doctrine->em->persist($dbVersion);
-			    $doctrine->em->flush();
+			require_once dirname(__FILE__) . '/dumps/' . $f;
+			$migrationMark = substr($f, 0, -4);
+			$queriesVariable = 'queries_' . $migrationMark;
+			foreach ($$queriesVariable as $q) {
+				$conn->executeQuery('SET FOREIGN_KEY_CHECKS=0');
+				$conn->executeQuery($q);
 			}
-		} catch(Exception $e) {
-	        $date = new DateTime("now");
-	        $file = fopen(APPPATH . 'logs' . DIRECTORY_SEPARATOR . 'log_migration_' . $date->format("Y-m-d") . '.log', 'a+');
-	        fwrite($file, $date->format("Y-m-d H:i:s") . " | Error in file $migrationFile : " . $e->getMessage() . PHP_EOL);
+			$dbVersion->setValue($migrationMark);
+		    $doctrine->em->flush();
+		} catch(\Exception $e) {
+	        $date = new \DateTime("now");
+	        $file = fopen(APPPATH . 'logs/log_migration_' . $date->format("Y-m-d") . '.log', 'a+');
+	        fwrite($file, $date->format("Y-m-d H:i:s") . ' | Error in file $migrationFile : ' . $e->getMessage() . PHP_EOL);
 	        fclose($file);   
 		}
 	}
+	
+	$dbRunning->setValue('0');
+	$doctrine->em->flush();
 }
